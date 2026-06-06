d66a63b98c9a0b6e37b61ee1acd37d83806f14a2
qmzlr <drozdovvalerij61@gmail.com>
2026-06-06 23:54:06 +0400

    12

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title inertia>{{ config('app.name', 'Playnote') }}</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
         @viteReactRefresh
         @vite('resources/js/inertia.tsx')
         @inertiaHead
